5cca08572523d67c148930c7002a8d5a298a37aa
Alex Erofeev <xnet141@gmail.com>
2025-11-29 02:19:24 +0300

    rails set

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+    def index
+        # render 'pages/index'
+        # @username = params[:username]
+    end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,6 @@
 // Entry point for the build script in your package.json
-import * as bootstrap from "bootstrap"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()
